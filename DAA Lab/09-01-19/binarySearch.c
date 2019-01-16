0ba5d3555f4964d14586443cb51ba023977e4145
AdityaSher <adityasher2000@gmail.com>
2019-01-16 11:47:49 +0530

    Read numbers from file function completed

--- DAA Lab/09-01-19/binarySearch.c
+++ DAA Lab/09-01-19/binarySearch.c
@@ -24,8 +24,9 @@ fclose(fp1);
 return num;
 }
 
-int readArray(int num)
+int* readArray(int num)
 	{
+	int *arr;
 	arr=(int*)malloc(num * sizeof(int)); 
 	// We need a dynamic array so that we can allot large amount of numbers
 	if (arr==NULL)
@@ -33,26 +34,38 @@ int readArray(int num)
 		printf("Error: Array has not been allocated! \n");
 	}
 
-	FILE *fp1=fopen("ipfile.txt","r");
+	FILE *ptr=fopen("ipfile.txt","r");
 	int i=0;
-	while()
+	int z=0;
+
+/* Refer: https://stackoverflow.com/questions/1835986/how-to-use-eof-to-run-through-a-text-file-in-c */
+
+	while(!feof(ptr)) //workaround
 	{
-		int z=0;
-		printf("Entered while loop");
-		fscanf(fp1,"%d",&z);
-		ptr+i=z;
+		fscanf(ptr,"%d\n",&z);
+		//printf("\n%d\n",z); Was used to verify whether reading was happening properly
+		int *temp=arr+i;
+		i++;
+		*temp=z;
+		// printf("%d",*temp); Was used to verify whether assignment of z to arr+i was happening properly
 	}
 	
-
-
-return *arr
+	
+return arr;
 	}
 
 int main()
 {
 	int num=numberOfNumbers();
-	readArray(num);
-	
+	int *rnd_arr=readArray(num);
+	int i=0;
+	int* temp=rnd_arr;
+	while(temp!=NULL)
+	{
+		temp=rnd_arr+i;
+		printf("%d\n",*temp);		
+		i++;
+	}
 
  	return 0;
 }
