e448aec64800381045722ec4f917a38f44812d74
adityasher <anshertg@gmail.com>
2019-01-26 16:53:38 +0530

    remove check for pin no 15

--- Test/test.py
+++ Test/test.py
@@ -6,12 +6,6 @@ io.setup(22, io.IN)
 
 while(1):
 
-    if io.input(15):
-        print('Input was 15HIGH')
-        break
-        print("15")
-    else:
-        print('Input was 15LOW')
     
     if io.input(22):
         print('Input was 22HIGH')
