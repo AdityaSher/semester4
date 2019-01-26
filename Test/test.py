923b85d0e5a87d93890143f124bb8d4cf67719af
adityasher <anshertg@gmail.com>
2019-01-26 15:52:06 +0530

    try alternate method to read pin

--- Test/test.py
+++ Test/test.py
@@ -1,10 +1,19 @@
 import RPi.GPIO as io
 
 io.setmode(io.BOARD)
-x=io.setup(15, io.IN)
-#x = io.output(15, not io.input(15))
+x=io.setup(15, io.OUT)
 
+if io.input(15):
+    print('Input was HIGH')
+else:
+    print('Input was LOW')
 
+if io.input(22):
+    print('Input was HIGH')
+else:
+    print('Input was LOW')
+
+"""
 while(1):
 
     if(x == 1):
@@ -12,4 +21,4 @@ while(1):
         print("Success")
         break
     print("f")
-
+"""
