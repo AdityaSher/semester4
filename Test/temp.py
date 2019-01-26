400c8eaed1ea2f476092c916c00f054135467ecd
adityasher <anshertg@gmail.com>
2019-01-26 16:33:15 +0530

    revert dependencies

--- Test/temp.py
+++ Test/temp.py
@@ -23,7 +23,7 @@ import sys
 
 import Adafruit_DHT
 
-import request 
+import urllib.request 
 
 
 # Parse command line parameters.
@@ -52,7 +52,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 if humidity is not None and temperature is not None:
     data = 'http://hackhire.azurewebsites.net/api/iot/data/{Temparature:"'+temperature+'", Humidity:"' +humidity+'", From: "dht"}'
     
-    contents = request.urlopen(data).read()  # for get protocol
+    contents = urllib.request.urlopen(data).read()  # for get protocol
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
