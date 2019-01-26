0410867f058acb2fcec77dc4a5fb2c9c0e84bffe
adityasher <anshertg@gmail.com>
2019-01-26 15:33:40 +0530

    syntax error corrected

--- Test/test.py
+++ Test/test.py
@@ -2,16 +2,14 @@ import RPi.GPIO as io
 
 io.setmode(io.BOARD)
 io.setup(22, io.OUT)
-
 x = io.output(22, not io.input(22))
 
 
 while(1):
 
     if(x == 1):
-   
         print(x)
         print("Success")
         break
-    print("f");
+    print("f")
 
