02098646fda810a253500026064d2ffc74f70d0a
adityasher <anshertg@gmail.com>
2019-01-26 16:10:22 +0530

    Reduce test conditions

--- Test/test.py
+++ Test/test.py
@@ -12,14 +12,14 @@ while(1):
         print('Input was LOW')
     """
     if io.input(22):
-        time.sleep(2)
+        #time.sleep(2)
         print('Input was HIGH')
         break
     
-    
+    """
     else:
         print('Input was LOW')
-        
+    """  
 
 """
 while(1):
