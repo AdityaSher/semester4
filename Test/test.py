f7c39072477898369369b2860dd007adf3f190fe
adityasher <anshertg@gmail.com>
2019-01-26 18:13:28 +0530

    remove commented part

--- Test/test.py
+++ Test/test.py
@@ -7,21 +7,3 @@ io.setup(22, io.IN)
 x=io.input(22)
 print(x)
 time.sleep(2)
-    
-    """if io.input(22):
-        print('Input was 22HIGH')
-        break
-        print("22")
-    """
-    """else:
-        print('Input was 22LOW')"""
-
-"""
-while(1):
-
-    if(x == 1):
-        print(x)
-        print("Success")
-        break
-    print("f")
-"""
