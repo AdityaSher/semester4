9f3d7b5e70e7f9a5b4780dfaeeb8c29a92e31470
adityasher <anshertg@gmail.com>
2019-01-26 18:55:52 +0530

    add while loop to temp program

--- Test/temp.py
+++ Test/temp.py
@@ -22,7 +22,7 @@
 import sys
 
 import Adafruit_DHT
-
+import time
 import urllib2
 import urllib
 import requests
@@ -40,32 +40,34 @@ else:
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
     sys.exit(1)
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+while(1):
+    time.sleep(10)
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
+    # Un-comment the line below to convert the temperature to Fahrenheit.
+    # temperature = temperature * 9/5.0 + 32
 
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
-if humidity is not None and temperature is not None:
-    
-    data1 = 'http://hackhire.azurewebsites.net/api/iot/data/%7BTemparature:"'+str(temperature)+'",Humadity:"'+str(humidity)+'",From:"dht"%7D'
+    # Note that sometimes you won't get a reading and
+    # the results will be null (because Linux can't
+    # guarantee the timing of calls to read the sensor).
+    # If this happens try again!
+    if humidity is not None and temperature is not None:
+        
+        data1 = 'http://hackhire.azurewebsites.net/api/iot/data/%7BTemparature:"'+str(temperature)+'",Humadity:"'+str(humidity)+'",From:"dht"%7D'
 
-    print(data1)
-    #contents = urllib2.urlopen(data).read()
+        print(data1)
+        #contents = urllib2.urlopen(data).read()
 
-    
+        
 
-    #payload = open("request.json")
-    headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
-    r = requests.get(data1, headers=headers)
+        #payload = open("request.json")
+        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        r = requests.get(data1, headers=headers)
 
 
-    print('Temp={0}*  Humidity={1}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    sys.exit(1)
+        print('Temp={0}*  Humidity={1}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+        sys.exit(1)
